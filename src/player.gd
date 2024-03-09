29e5114d32a7c6cc7c569ab19033c7276a532b4b
William McPherson <willmcpherson2@gmail.com>
2024-03-09 17:27:42 +1100

    Move player towards mouse click

---
+++ src/player.gd
@@ -0,0 +1,22 @@
+# godot 4.1
+
+extends CharacterBody2D
+
+const SPEED: float = 10000
+
+var target: Vector2 = Vector2()
+
+func _ready() -> void:
+	position = Vector2(0, 0)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
+
+func _process(delta: float) -> void:
+	var direction: Vector2 = position.direction_to(target)
+	velocity = direction * SPEED * delta
+	if position.distance_to(target) > 10:
+		move_and_slide()
+	else:
+		velocity = Vector2(0, 0)
