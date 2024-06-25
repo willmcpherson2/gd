ef99261a21b61c39205c5ea6a735df65b93183ef
William McPherson <willmcpherson2@gmail.com>
2024-06-25 21:22:19 +1000

    Add types

--- src/main.gd
+++ src/main.gd
@@ -1,12 +1,12 @@
 extends Node
 
-func _ready():
-	var player_sprite = Sprite2D.new()
+func _ready() -> void:
+	var player_sprite: Sprite2D = Sprite2D.new()
 	player_sprite.texture = load("res://assets/player.png")
 
-	var player_script = load("res://src/player.gd")
+	var player_script: Resource = load("res://src/player.gd")
 
-	var player = CharacterBody2D.new()
+	var player: CharacterBody2D = CharacterBody2D.new()
 	player.add_child(player_sprite)
 	player.set_script(player_script)
 
